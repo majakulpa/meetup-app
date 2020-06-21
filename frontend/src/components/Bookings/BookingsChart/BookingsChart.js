8b03ad1484720f5536d69c780188bdd19563d2a5
majakulpa <majaweronikakulpa@gmail.com>
2020-06-22 00:53:50 +1000

    add react chart for bookings

--- frontend/src/components/Bookings/BookingsChart/BookingsChart.js
+++ frontend/src/components/Bookings/BookingsChart/BookingsChart.js
@@ -1,26 +1,51 @@
 import React from 'react'
+import { Doughnut } from 'react-chartjs-2'
 
 const BOOKINGS_BUCKETS = {
-  Free: 0,
-  Cheap: 10,
-  Normal: 100,
-  Expensive: 100000
+  Free: {
+    min: 0,
+    max: 0
+  },
+  Cheap: {
+    min: 0.01,
+    max: 9.99
+  },
+  Normal: {
+    min: 10.0,
+    max: 99.99
+  },
+  Expensive: {
+    min: 100.0,
+    max: 1000000
+  }
 }
 
 const bookingsChart = props => {
-  const output = {}
+  const chartData = { labels: [], datasets: [] }
+  let values = []
   for (const bucket in BOOKINGS_BUCKETS) {
     const filteredBookingsCount = props.bookings.reduce((prev, current) => {
-      if (current.event.price <= BOOKINGS_BUCKETS[bucket]) {
+      if (
+        current.event.price >= BOOKINGS_BUCKETS[bucket].min &&
+        current.event.price <= BOOKINGS_BUCKETS[bucket].max
+      ) {
         return prev + 1
       } else {
         return prev
       }
     }, 0)
-    output[bucket] = filteredBookingsCount
+    values.push(filteredBookingsCount)
+    chartData.labels.push(bucket)
+    chartData.datasets = [
+      {
+        data: values,
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+      }
+    ]
   }
-  console.log(output)
-  return <div>chart</div>
+
+  return <Doughnut data={chartData} />
 }
 
 export default bookingsChart
